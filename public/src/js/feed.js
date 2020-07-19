90231ef215c62b993f1fa40c7503f6e3532e397e
unknown <you@example.com>
2020-07-19 06:45:00 +0530

    add native android features

---
+++ public/src/js/feed.js
@@ -0,0 +1,206 @@
+var shareImageButton = document.querySelector('#share-image-button');
+var createPostArea = document.querySelector('#create-post');
+var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+var sharedMomentsArea = document.querySelector('#shared-moments');
+var form = document.querySelector('form');
+var titleInput = document.querySelector('#title');
+var locationInput = document.querySelector('#location');
+function openCreatePostModal() {
+  createPostArea.style.display = 'block';
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+
+    deferredPrompt.userChoice.then(function(choiceResult) {
+      console.log(choiceResult.outcome);
+
+      if (choiceResult.outcome === 'dismissed') {
+        console.log('User cancelled installation');
+      } else {
+        console.log('User added to home screen');
+      }
+    });
+
+    deferredPrompt = null;
+  }
+
+  // if ('serviceWorker' in navigator) {
+  //   navigator.serviceWorker.getRegistrations()
+  //     .then(function(registrations) {
+  //       for (var i = 0; i < registrations.length; i++) {
+  //         registrations[i].unregister();
+  //       }
+  //     })
+  // }
+}
+
+function closeCreatePostModal() {
+  createPostArea.style.display = 'none';
+}
+
+shareImageButton.addEventListener('click', openCreatePostModal);
+
+closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+
+// Currently not in use, allows to save assets in cache on demand otherwise
+function onSaveButtonClicked(event) {
+  console.log('clicked');
+  if ('caches' in window) {
+    caches.open('user-requested')
+      .then(function(cache) {
+        cache.add('https://httpbin.org/get');
+        cache.add('/src/images/sf-boat.jpg');
+      });
+  }
+}
+
+function clearCards() {
+  while(sharedMomentsArea.hasChildNodes()) {
+    sharedMomentsArea.removeChild(sharedMomentsArea.lastChild);
+  }
+}
+
+function createCard(data) {
+  var cardWrapper = document.createElement('div');
+  cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
+  var cardTitle = document.createElement('div');
+  cardTitle.className = 'mdl-card__title';
+  cardTitle.style.backgroundImage = 'url(' + data.image + ')';
+  cardTitle.style.backgroundSize = 'cover';
+  cardTitle.style.height = '180px';
+  cardWrapper.appendChild(cardTitle);
+  var cardTitleTextElement = document.createElement('h2');
+  cardTitleTextElement.style.color = 'white';
+  cardTitleTextElement.className = 'mdl-card__title-text';
+  cardTitleTextElement.textContent = data.title;
+  cardTitle.appendChild(cardTitleTextElement);
+  var cardSupportingText = document.createElement('div');
+  cardSupportingText.className = 'mdl-card__supporting-text';
+  cardSupportingText.textContent = data.location;
+  cardSupportingText.style.textAlign = 'center';
+  // var cardSaveButton = document.createElement('button');
+  // cardSaveButton.textContent = 'Save';
+  // cardSaveButton.addEventListener('click', onSaveButtonClicked);
+  // cardSupportingText.appendChild(cardSaveButton);
+  cardWrapper.appendChild(cardSupportingText);
+  componentHandler.upgradeElement(cardWrapper);
+  sharedMomentsArea.appendChild(cardWrapper);
+}
+
+function updateUI(data) {
+  clearCards();
+  for (var i = 0; i < data.length; i++) {
+    createCard(data[i]);
+  }
+}
+
+var url = 'https://pwagram-14145.firebaseio.com/posts.json';
+var networkDataReceived = false;
+
+fetch(url)
+  .then(function(res) {
+    return res.json();
+  })
+  .then(function(data) {
+    networkDataReceived = true;
+    console.log('From web', data);
+    var dataArray = [];
+    for (var key in data) {
+      dataArray.push(data[key]);
+    }
+    updateUI(dataArray);
+  });
+
+// if ('caches' in window) {
+//   caches.match(url)
+//     .then(function(response) {
+//       if (response) {
+//         return response.json();
+//       }
+//     })
+//     .then(function(data) {
+//       console.log('From cache', data);
+//       if (!networkDataReceived) {
+//         var dataArray = [];
+//         for (var key in data) {
+//           dataArray.push(data[key]);
+//         }
+//         updateUI(dataArray)
+//       }
+//     });
+// }
+if('indexedDB' in window){
+  readAllData('posts')
+  .then(function(data){
+    if(!networkDataReceived){
+      console.log('from cache',data);
+      updateUI(data);
+    }
+  });
+}
+
+
+function sendData(){
+  fetch('https://pwagram-14145.firebaseio.com/posts.json',{
+    method:'POST',
+    headers:{
+      'Content-Type' : 'application/json',
+      'Accept' : 'apllication/json'
+    },
+    body: JSON.stringify({
+      id: new Date().toISOString(),
+      title: titleInput.value,
+      location : locationInput.value,
+      image:'https://firebasestorage.googleapis.com/v0/b/pwagram-14145.appspot.com/o/Lorenzo_Garcia_tn.jpg?alt=media&token=fbbdefa5-ce7c-4ef5-a6dc-e17004580c49'
+
+    })
+  })
+  .then(function(res){
+    console.log('send data',res);
+    fetch(url)
+      .then(function(res) {
+        return res.json();
+      })
+      .then(function(data) {
+        networkDataReceived = true;
+        console.log('From web', data);
+        var dataArray = [];
+        for (var key in data) {
+          dataArray.push(data[key]);
+        }
+        updateUI(dataArray);
+      });
+  })
+}
+
+form.addEventListener('submit',function(event){
+  event.preventDefault();
+  if(titleInput.value.trim()==='' || locationInput.value.trim()===''){
+    alert('please add the fieids');
+    return;
+  }
+  closeCreatePostModal();
+  if('serviceWorker' in navigator && 'syncManager' in window){
+    navigator.serviceWorker.ready
+    .then(function(sw){
+      var post = {
+        id : new Data().toISOString(),
+        title: titleInput.value,
+        location: locationInput.value
+      };
+      writeData('sync-posts',post)
+      .then(function(){
+        return sw.sync.register('sync-new-posts');
+      })
+      .then(function(){
+        var snackbarContainer = document.querySelector('#confirmation-toast');
+        var data = {message: 'Your Post was saved for syncing!'};
+        snackbarContainer.MaterialSnackbar.showSnackbar(data);
+      })
+      .catch(function(err){
+        console.log(err);
+      });
+    });
+  }else{
+    sendData();
+  } 
+});
